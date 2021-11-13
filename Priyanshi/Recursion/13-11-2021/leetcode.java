b1b0404389bdb30bde4db9f13feab0a69d7dbaee
sirohipriya <92969307+sirohipriya@users.noreply.github.com>
2021-11-13 16:19:00 +0530

    Update leetcode.java

--- Priyanshi/Recursion/13-11-2021/leetcode.java
+++ Priyanshi/Recursion/13-11-2021/leetcode.java
@@ -109,3 +109,26 @@ public class Solution {
 
 
 
+class Solution {
+    public int[] countBits(int n) {
+        int[] ans = new int[n + 1];
+        for(int i = 0; i <= n; i++){
+            ans[i] = bits(i);
+            
+        }
+        return ans;
+    }
+    public int bits(int n) {
+        int count = 0;
+        while(n != 0){
+            n = (n & (n-1));
+            count++;
+        }
+        
+        return count;
+    }
+}
+
+
+
+//====================================================================================
