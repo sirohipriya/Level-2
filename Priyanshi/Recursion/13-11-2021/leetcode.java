063928cf0434abc22b7d35ee3b38baa71af5dda5
sirohipriya <92969307+sirohipriya@users.noreply.github.com>
2021-11-13 16:02:58 +0530

    Update leetcode.java

--- Priyanshi/Recursion/13-11-2021/leetcode.java
+++ Priyanshi/Recursion/13-11-2021/leetcode.java
@@ -66,4 +66,20 @@ class Solution {
 }
 
 
+//=================================================================================342. Power of 4===========================================================================
 
+
+
+class Solution {
+    public boolean isPowerOfFour(int n) {
+        if(n <= 0 || (n & (n-1)) != 0) return false;
+        
+        int count = 0;
+        while(n != 1){
+            n = n >>> 1;
+            count++;
+        }
+        
+        return ((count & 1) == 0);
+    }
+}
