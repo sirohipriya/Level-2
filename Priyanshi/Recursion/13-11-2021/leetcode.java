382771adfc76a1bc3426d5473e8941119467b109
sirohipriya <92969307+sirohipriya@users.noreply.github.com>
2021-11-13 14:09:25 +0530

    Update leetcode.java

--- Priyanshi/Recursion/13-11-2021/leetcode.java
+++ Priyanshi/Recursion/13-11-2021/leetcode.java
@@ -52,3 +52,18 @@ class Solution {
         solveSudoku(board, 0, cells, rows, cols, mat);
     }
 }
+
+
+
+//===================================================================================231. Power of Two=======================================================================
+
+
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        return(n > 0 &&(n & (n-1))==0);
+    }
+}
+
+
+
