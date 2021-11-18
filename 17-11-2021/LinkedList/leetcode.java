25d31785846e1b38c0e48a46e0813890ba3b5d71
sirohipriya <92969307+sirohipriya@users.noreply.github.com>
2021-11-19 01:35:20 +0530

    Update leetcode.java

--- 17-11-2021/LinkedList/leetcode.java
+++ 17-11-2021/LinkedList/leetcode.java
@@ -108,4 +108,30 @@ public void reorderList(ListNode head) {
         }
     
     
-    //===================================================================
+    //==============================================================================Unfold a List=========================================================================
+    
+    
+    
+    public static void unfold(ListNode head) {
+        if (head == null || head.next == null)
+            return;
+
+        ListNode c1 = head, c2 = head.next, nHead = c2;
+        while (c2 != null && c2.next != null) {
+            ListNode f1 = c2.next;
+
+            c1.next = f1;
+            c2.next = f1.next;
+
+            c1 = c1.next; // c1 = f1;
+            c2 = c2.next; // c2 = c1.next;
+        }
+
+        c1.next = null;
+        nHead = reverse(nHead);
+        c1.next = nHead;
+    }
+    
+    
+    
+    //======================================================================
