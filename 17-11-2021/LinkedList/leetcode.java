8dce44a7b817be588c1620be8a05de64532fa0fa
sirohipriya <92969307+sirohipriya@users.noreply.github.com>
2021-11-19 01:32:59 +0530

    Update leetcode.java

--- 17-11-2021/LinkedList/leetcode.java
+++ 17-11-2021/LinkedList/leetcode.java
@@ -63,7 +63,7 @@ public ListNode middleNode2(ListNode head) {
 public boolean isPalindrome(ListNode head) {
         if(head == null || head.next == null) return true;
         
-        ListNode mid = middleNode(head);
+        ListNode mid = middleNode2(head);
         ListNode nhead = reverseList(mid.next);
         
         while(nhead != null){
@@ -78,3 +78,34 @@ public boolean isPalindrome(ListNode head) {
         
         return true;
     }
+
+
+//==========================================================================143. Reorder List===============================================================================
+
+
+
+public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return;
+        
+        ListNode mid = middleNode2(head);
+        ListNode nhead = mid.next;
+        mid.next = null;
+        
+        nhead = reverseList(nhead);
+        
+        ListNode c1 = head;
+        ListNode c2 = nhead;
+        while(c2 != null){
+            ListNode f1 = c1.next;
+            ListNode f2 = c2.next;
+            
+            c1.next = c2;
+            c2.next = f1;
+            
+            c1 = f1;
+            c2 = f2;
+            
+        }
+    
+    
+    //===================================================================
