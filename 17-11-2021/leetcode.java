95997942f1106596009477bd0f3569f1ee08ef39
sirohipriya <92969307+sirohipriya@users.noreply.github.com>
2021-11-18 17:54:09 +0530

    Update leetcode.java

--- 17-11-2021/leetcode.java
+++ 17-11-2021/leetcode.java
@@ -40,4 +40,41 @@ class Solution {
 
 
 
-// ==============================================================
+//===================================================================== Mid of a node(even-> last of first half)==============================================================
+
+
+public ListNode middleNode2(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode fast = head;
+        ListNode slow = head;
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+    
+
+
+// =======================================================================234. Palindrome LinkedList=========================================================================
+
+
+
+public boolean isPalindrome(ListNode head) {
+        if(head == null || head.next == null) return true;
+        
+        ListNode mid = middleNode(head);
+        ListNode nhead = reverseList(mid.next);
+        
+        while(nhead != null){
+            if(head.val != nhead.val){
+                return false;
+            }
+                head = head.next;
+                nhead = nhead.next;
+        }
+        nhead = reverseList(nhead);
+        mid.next = nhead;
+        
+        return true;
+    }
